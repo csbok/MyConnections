4688e3c3cb3d3045606c7d47999610b3acf17b2a
csbok <winmain@gmail.com>
2018-06-15 00:29:26 +0900

    init

---
+++ MyConnections/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyConnections
+//
+//  Created by BokChangsoon on 2018. 6. 15..
+//  Copyright © 2018년 FineSoft.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
